aad494818028de0b75bc6b6eed0f6e8b258e367d
KarlaJulyana <karla@solarz.com.br>
2024-08-02 00:07:43 -0300

    chore: install socket io client

---
+++ src/components/websocketComponent.tsx
@@ -0,0 +1,38 @@
+// components/WebSocketComponent.js
+
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('ws://localhost:8000');  // Django WebSocket server address
+
+const WebSocketComponent = () => {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        socket.on('connect', () => {
+            console.log('Connected to WebSocket server');
+        });
+
+        socket.on('message', (data) => {
+            setMessage(data.message);
+        });
+
+        return () => {
+            socket.off('connect');
+            socket.off('message');
+        };
+    }, []);
+
+    const sendMessage = () => {
+        socket.send(JSON.stringify({ message: 'Hello from Next.js!' }));
+    };
+
+    return (
+        <div>
+            <button onClick={sendMessage}>Send Message</button>
+            <p>Received: {message}</p>
+        </div>
+    );
+};
+
+export default WebSocketComponent;
